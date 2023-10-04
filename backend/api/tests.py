74c2d63ec83d7262ca22a6c89b44fb5cda0b3df3
shkiperzero <ibazhov92@gmail.com>
2023-10-04 21:05:42 +0500

    fix test5

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -10,7 +10,7 @@ class TaskiAPITestCase(TestCase):
 
     def test_list_exists(self):
         """Проверка доступности списка задач."""
-        response = self.quest_client.get('/api/tasks/')
+        response = self.guest_client.get('/api/tasks/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_task_creation(self):
